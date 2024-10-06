62a19fcd92be3b63ef5bf777ce9335b14a6d41bc
Esmeralda <eruby1910@gmail.com>
2024-10-05 20:11:58 -0600

    geronio

--- PRUEBA/Form1.Designer.cs
+++ PRUEBA/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace PRUEBA
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(22, 68);
+            this.button1.Location = new System.Drawing.Point(291, 169);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(170, 77);
             this.button1.TabIndex = 0;
@@ -45,7 +45,7 @@ namespace PRUEBA
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(351, 95);
+            this.button2.Location = new System.Drawing.Point(99, 159);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
