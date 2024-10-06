06e5ddd38d207e055b5f089c90b10d6366083c4d
Esmeralda <eruby1910@gmail.com>
2024-10-05 20:05:59 -0600

    holi

--- PRUEBA/Form1.Designer.cs
+++ PRUEBA/Form1.Designer.cs
@@ -41,10 +41,11 @@ namespace PRUEBA
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(237, 68);
+            this.button2.Location = new System.Drawing.Point(351, 95);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
